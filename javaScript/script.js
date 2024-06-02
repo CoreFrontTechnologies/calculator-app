31f3583494fb061fbec4dd36b48c47387d994deb
coreFront Technologies <sayyidyahaya888@gmail.com>
2024-06-02 15:22:20 +0100

    This is my first commit

---
+++ javaScript/script.js
@@ -0,0 +1,10 @@
+function addToDisplay(value) {
+    document.getElementById("display").value += value
+}
+
+
+function calculate() {
+    let expression = document.getElementById("display").value;
+    let result = eval(expression);
+    document.getElementById("display").value = result;
+}
